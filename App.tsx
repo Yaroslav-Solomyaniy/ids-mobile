7d136c23fcc26921342223e50c3acb267735f51c
Yaroslav Solomyaniy <soman74geeev@gmail.com>
2023-02-26 15:46:20 +0200

    add svg in project

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import styles from './app.scss';
+import SVGImg from './logo.svg';
 
 const App = () => {
   return (
     <View>
       <Text style={styles.text}>App</Text>
+      <SVGImg width={200} height={200} />
     </View>
   );
 };
--- declaration.d.ts
+++ declaration.d.ts
@@ -1,1 +1,13 @@
 declare module '*';
+
+declare module '*.svg' {
+  import React from 'react';
+  import {SvgProps} from 'react-native-svg';
+  const content: React.FC<SvgProps>;
+  export default content;
+}
+
+declare module '*.scss' {
+  const content: Record<string, string>;
+  export default content;
+}
